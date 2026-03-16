ffac125a7c4f94f5e0becae976fd1559c151c99e
sooji7435 <bys0634@naver.com>
2026-03-16 13:59:14 +0900

    feat: fcm 추가
    - fcm 기능 추가

--- NearlyApp.swift
+++ NearlyApp.swift
@@ -6,14 +6,27 @@
 //
 
 import SwiftUI
+import UserNotifications
 import KakaoSDKCommon
 import NidThirdPartyLogin
 import Firebase
+import FirebaseMessaging
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in
+            
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
         
         NidOAuth.shared.initialize(
             appName: "Nearly",
@@ -23,6 +36,29 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         )
         return true
     }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+    
+    func application(_ application: UIApplication,
+                         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            print("APNS token: \(deviceToken)")
+            Messaging.messaging().apnsToken = deviceToken
+        }
 }
 
 @main
