5d619613194d8ade3bbcf6eecf6ebdd27a2652e1
sooji7435 <bys0634@naver.com>
2026-03-16 17:56:37 +0900

    chore: 코드 정리
    - 코드 정리

--- NearlyApp.swift
+++ NearlyApp.swift
@@ -20,12 +20,9 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         UNUserNotificationCenter.current().delegate = self
         
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in
-            
-        }
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
         
         application.registerForRemoteNotifications()
-        
         Messaging.messaging().delegate = self
         
         NidOAuth.shared.initialize(
@@ -37,43 +34,38 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         return true
     }
     
+    // FCM 토큰 발급 / 갱신 시 호출
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
-        print("토큰 저장됨")
-        UserDefaults.standard.set(fcmToken, forKey: "fcmToken")
+        guard let token = fcmToken else { return }
+        print("FCM 토큰 저장됨: \(token)")
+        UserDefaults.standard.set(token, forKey: "fcmToken")
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    // 앱 포그라운드 상태에서도 알림 표시
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                 willPresent notification: UNNotification,
+                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.list, .banner])
     }
     
+    // APNs 토큰 → Firebase 전달
     func application(_ application: UIApplication,
-                         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-            print("APNS token: \(deviceToken)")
-            Messaging.messaging().apnsToken = deviceToken
-        }
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
 
 @main
 struct Nearly: App {
-    // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    //Manager
+    // Manager
     @StateObject var locationManager: LocationManager = LocationManager()
     @StateObject var userManager: UserManager = UserManager()
     @StateObject var recruitManager: RecruitManager = RecruitManager()
     
-    //ViewModel
+    // ViewModel
     @StateObject var authViewModel: AuthenticationViewModel = AuthenticationViewModel()
     @StateObject var appStateViewModel: AppStateViewModel = AppStateViewModel()
     @StateObject var runningViewModel: RunningViewModel = RunningViewModel()
@@ -82,32 +74,26 @@ struct Nearly: App {
         KakaoSDK.initSDK(appKey: "8ccbb76509da0aaf5a266b99bb5a8521")
     }
     
-    
     var body: some Scene {
         WindowGroup {
             RootView()
-            
-            //Manager
+                // Manager
                 .environmentObject(locationManager)
                 .environmentObject(userManager)
                 .environmentObject(recruitManager)
-            
-            //ViewModel
+                // ViewModel
                 .environmentObject(authViewModel)
                 .environmentObject(appStateViewModel)
                 .environmentObject(runningViewModel)
                 .onAppear {
                     appStateViewModel.checkLogin()
                     
+                    // 자동 로그인 시 유저 정보 복원
                     if let userId = UserDefaults.standard.string(forKey: "userId") {
-                            userManager.user.id = userId
-                            userManager.fetchUserInfo(userID: userId) { _ in }
-                        }
+                        userManager.user.id = userId
+                        userManager.fetchUserInfo(userID: userId) { _ in }
+                    }
                 }
         }
-        
-        
     }
-    
-    
 }
